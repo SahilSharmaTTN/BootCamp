8b21b3c34cbe5517bf16765525c89f80fa9d0cc3
Sahil Sharma <sahil.sharma@tothenew.com>
2022-10-02 22:24:23 +0530

    Assignment 5 changes

---
+++ Assignment5/src/Question11.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+record Student(String name, Integer id, Integer age){
+    static int counter =0;
+    static void incrementCounter(){
+        counter++;
+    }
+
+    Student{
+        Objects.requireNonNull(name);
+        incrementCounter();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+
+}
+
+public class Question11 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Sahil",1,23);
+        Student s2 = new Student("Sahil",1,23);
+        Student s3 = new Student("Sahil",1,23);
+
+        System.out.println(Student.counter);
+
+
+
+    }
+}
